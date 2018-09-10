5747a430cdb6a0a1f0c953c652383a2dc0df6163
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 16:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function theBeatlesPlay (musicians, instruments) {
   var arr = [];
   for (let i = 0; i < musicians.length; i++) {
-    arr.push(musicians[i] + " plays " + instruments[0])
-
-    return arr;
+    arr.push(musicians[i] + " plays " + instruments[i]);
   }
+
+  return arr;
 }
