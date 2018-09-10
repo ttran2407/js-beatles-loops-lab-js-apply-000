7ac9580357092a4a0106d18b65bedec079092edb
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 16:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay (musicians, instruments) {
 function johnLennonFacts (facts) {
   let i = 0;
   while (i < facts.length) {
-    facts[i] + "!!!";
+    facts[i] = facts[i] + "!!!";
     i++;
   }
 
