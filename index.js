0fe78d25a78f891b2ceba56809ad12d9e83fc862
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 16:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var arr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + "plays" + instruments[0])
+
+    return arr
+  }
+}
